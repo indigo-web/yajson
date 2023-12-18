197154cadb931f22b225b006216ee07d3238e7c2
a very fake floordiv <real.floordiv@gmail.com>
2023-12-18 15:37:24 +0100

    added nested structs support via pointers

--- json.go
+++ json.go
@@ -6,6 +6,8 @@ import (
 	"github.com/indigo-web/utils/uf"
 	"github.com/indigo-web/yajson/flect"
 	"github.com/romshark/jscan/v2"
+	"reflect"
+	"strings"
 	"unsafe"
 )
 
@@ -24,7 +26,7 @@ type JSON[T any] struct {
 // New returns a JSON parser instance for one specific model, defined via the generic
 func New[T any]() *JSON[T] {
 	return &JSON[T]{
-		model:  flect.NewModel[T](nil),
+		model:  flect.NewModel[T](new(pathDeserializer)),
 		buffer: buffer.New(BufferSpaceMin, BufferSpaceMax),
 	}
 }
@@ -32,12 +34,12 @@ func New[T any]() *JSON[T] {
 // Parse parses the passed JSON and stores the values into a new instance of the model
 func (j *JSON[T]) Parse(input string) (result T, err error) {
 	jsonErr := jscan.Scan(input, func(i *jscan.Iterator[string]) (exit bool) {
-		key := i.Key()
-		if len(i.Key()) == 0 || i.ValueType() != jscan.ValueTypeString {
+		key := i.Pointer()
+		if len(key) == 0 || i.ValueType() != jscan.ValueTypeString {
 			return false
 		}
 
-		field, found := j.model.Field(key[1 : len(key)-1])
+		field, found := j.model.Field(key)
 		if !found {
 			return false
 		}
@@ -58,9 +60,30 @@ func (j *JSON[T]) Parse(input string) (result T, err error) {
 		err = jsonErr
 	}
 
-	// this won't destroy strings we've written, however on next call they may be overridden
-	// by new ones
+	// this doesn't destroy data we've written, but will override them on the next call
 	j.buffer.Clear()
 
 	return result, err
+}
+
+type pathDeserializer struct {
+	stack []string
+}
+
+func (p *pathDeserializer) Descend(field reflect.StructField) {
+	p.stack = append(p.stack, field.Name)
+}
+
+func (p *pathDeserializer) Ascend() {
+	p.stack = p.stack[:len(p.stack)-1]
+}
+
+func (p *pathDeserializer) Visit(field reflect.StructField) (name string) {
+	name = "/"
+
+	if len(p.stack) > 0 {
+		name += strings.Join(p.stack, "/") + "/"
+	}
+
+	return name + field.Name
 }
